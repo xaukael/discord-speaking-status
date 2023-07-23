391df9f3cf39d975534cb2c357cca2c61b2a8db2
xaukael <37848032+xaukael@users.noreply.github.com>
2023-07-22 19:28:05 -0500

    Add files via upload

--- discord-speaking-status.js
+++ discord-speaking-status.js
@@ -1,28 +1,29 @@
 listenForDiscordEvents = function (e) {
-  let user = game.users.find(u=> u.flags["discord-speaking-status"]?.id == e.data.user_id)
+  let user = game.users.find(u=> u.flags["discord-speaking-status"]?.id == e.data.userId)
+  /*
   if (!user) {
     
-    let id = 'assign-' + e.data.user_id;
+    let id = 'assign-' + e.data.userId;
     //console.log($(`#${id}`).length)
     if ($(`#${id}`).length) {
       if (e.data.evt=="SPEAKING_START") return $(`#${id}`).css({outline: '3px solid #3BA53B'});
       if (e.data.evt=="SPEAKING_STOP") return $(`#${id}`).css({outline: 'unset'});
     }
     new Dialog({
-      title: e.data.user_id,
+      title: e.data.userId,
       content: game.users.filter(u=> !u.flags["discord-speaking-status"]?.id).reduce((a,u)=>a+=`<option value="${u.id}">${u.name}</option>`,`<select name="user">`)+`</select>`,
       buttons: {
         confirm: {label:'confirm', callback: (html)=>{
-          game.users.get(html.find('select').val()).setFlag('discord-speaking-status', 'id', e.data.user_id)
+          game.users.get(html.find('select').val()).setFlag('discord-speaking-status', 'id', e.data.userId)
         }}
       },
       default: 'confirm'
     },{id}).render(true)
     return;
-  };
+  };*/
   Hooks.call('discordSpeakingEvent', e.data)
   let tokens = user.character?.getActiveTokens();
-  if (e.data.evt=="SPEAKING_START") {
+  if (e.data.speaking) {
     $(`#player-list > li[data-user-id="${user.id}"] span:first-child`).css({outline: '5px solid #3BA53B'});
     tokens.forEach(t => {
       
@@ -30,7 +31,7 @@ listenForDiscordEvents = function (e) {
       $(`#token-action-bar li[data-token-id="${t.id}"]`).css({outline: '3px solid #3BA53B'});
     });
   }
-  if (e.data.evt=="SPEAKING_STOP") {
+  if (!e.data.speaking) {
     $(`#player-list > li[data-user-id="${user.id}"] span:first-child`).css({outline: 'unset'});
     tokens.forEach(t => { 
       $('#hud').find(`div.speaking-token-marker.${t.id}`).remove(); 
@@ -59,7 +60,31 @@ openDiscordWindow = async function () {
   //await window.navigator.clipboard.writeText(code);
   channel = game.settings.get("discord-speaking-status", "channel");
   let parts = channel.split('/');
-  window.open(`https://streamkit.discord.com/overlay/voice/${parts[4]}/${parts[5]}`)
+  window.open(channel);
+  
+  new Dialog({title:'Companion Script', content: `<p>copy this<br> paste it into the console (F12) of the discord window<br>execute to monitor speaking status</p><textarea>
+const mutList = new MutationObserver((mutations, mut) => {
+    for (let m of mutations) {
+        if (!m.target.className.includes('userAvatar')) continue;
+        let data = {};
+        if (!mutations) return;
+        let match = mutations[0].target.style.backgroundImage.match(/avatars\\/([0-9])+\\//g);
+        if (!match) return;
+        let ua = match[0];
+        data.userId = ua.substring(ua.indexOf('/')+1, ua.lastIndexOf('/'));
+        data.speaking = mutations[0].target.className.includes('Speaking');
+        console.log("emmitting event", data);
+        window.opener.postMessage(data, '*');
+    }
+});
+document.querySelectorAll('li[class*="containerDefault"]').forEach((li)=>{
+    mutList.observe(li, {
+        'childList': true,
+        'subtree': true,
+        'attributes': true
+    });
+});
+</textarea>`, buttons:{}}).render(true);
 }
 Hooks.once("init", async () => {
   
@@ -76,7 +101,7 @@ Hooks.once("init", async () => {
 });
 
 Hooks.on('renderSettings', (app, html)=>{
-  html.find('#settings-access').prepend($(`<button><i class="fa-brands fa-discord"></i> Open Discord StreamKit</button>`).click(function(){openDiscordWindow()}))
+  html.find('#settings-access').prepend($(`<button><i class="fa-brands fa-discord"></i> Open Discord Voice Channel</button>`).click(function(){openDiscordWindow()}))
 })
 
 Hooks.on('renderUserConfig', (app, html, data)=>{
